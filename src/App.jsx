bced6c476f22832b04790eac3a8caf7b2be4061a
Blair Burke <blairburke@outlook.com>
2021-10-28 15:49:21 -0230

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Calculator } from "./components";
+import "./styles/styles.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div className="calculator-container">
+      <div className="displays-container">
+        <div className="output-display">Output</div>
+        <div className="input-display">Input</div>
+      </div>
+      <div className="buttons-container">
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
